f79cd9038059f2f630fae84eaf52de454b17c449
danyrfreitas <danyrfreitas@gmail.com>
2026-06-05 19:44:21 +0000

    feat: adição do slideshow

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ links.forEach(link => {
     }
   });
 });
+
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);
